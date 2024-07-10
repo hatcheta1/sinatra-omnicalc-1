3de14dac1824e7dc79ebc9c2910b849c68e8c12f
Ashanti Hatchett <ahatchett02@gmail.com>
2024-07-10 17:14:09 +0000

    added ability to calculate square of user submitted number

--- app.rb
+++ app.rb
@@ -2,8 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square_new)
+end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get '/square/results' do
+  @user_num = params.fetch("users_number").to_f
+  
+  @square_result = @user_num ** 2
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  "square_root"
+end
+
+get("/payment/new") do
+  "payment"
+end
+
+get("/random/new") do
+  "random"
 end
